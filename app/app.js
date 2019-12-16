422d1f4f11b1ea59cce63d28915307b8c2ed1f8f
Maciej Preus <mpreus@onet.eu>
2019-12-16 23:36:10 +0100

    Add more functions to app.js incl. button to empty the list of items

--- app/app.js
+++ app/app.js
@@ -15,18 +15,20 @@ class Shop extends React.Component {
 	render() {
 		return (
 			<div>
-  				<ShopItem title="Skarpetki" description="Bawełniane w czerwoną kratę" onBuy={this.handleBuy}/>
-  				<ShopItem title="Koszula" description="Jedwabna, z kołnierzykiem typu straight, kremowa" onBuy={this.handleBuy}/>
-  				<ShopItem title="Krawat" description="Wzorzysty, żakardowy, miodowy" onBuy={this.handleBuy}/>
-  				<ShopItem title="Spinka do krawata" description="W formie pióra, ecru" onBuy={this.handleBuy}/>
-  				<ShopItem title="Poszetka" description="Jedwabna, gładka, zieleń butelkowa" onBuy={this.handleBuy}/>
-  				<ShopItem title="Muszka" description="Średnia, typu pre-tied, bordowa" onBuy={this.handleBuy}/>
-  				<ShopItem title="Spinki do mankietów" description="W formie okrętowego koła ratunkowego" onBuy={this.handleBuy}/>
+				<h2>Elementy garderoby wg Twoich upodobań</h2>
+  				<ShopItem title="Skarpetki" description="Bawełniane w czerwoną kratę" price="45" onBuy={this.handleBuy}/>
+  				<ShopItem title="Koszula" description="Jedwabna, z kołnierzykiem typu straight, kremowa" price="245" onBuy={this.handleBuy}/>
+  				<ShopItem title="Krawat" description="Wzorzysty, żakardowy, miodowy" price="167" onBuy={this.handleBuy}/>
+  				<ShopItem title="Spinka do krawata" description="W formie pióra, ecru" price="199" onBuy={this.handleBuy}/>
+  				<ShopItem title="Poszetka" description="Jedwabna, gładka, zieleń butelkowa" price="98" onBuy={this.handleBuy}/>
+  				<ShopItem title="Muszka" description="Średnia, typu pre-tied, bordowa" price="189" onBuy={this.handleBuy}/>
+  				<ShopItem title="Spinki do mankietów" description="W formie okrętowego koła ratunkowego" price="139" onBuy={this.handleBuy}/>
+  				<h2>Zawartość koszyka</h2>
   				<ul> 
-    				{this.state.shoppingList.map( (title, idx) => <li key={idx}>{title}</li>)}
+    				{this.state.shoppingList.map( (title, idx, price) => <li key={idx}>{title}</li>)}
   				</ul>     {/*dla każdego przedmiotu (i indeksu) utwórz 'li' z atrybutami*/}
-  				
-			</div> 
+  				{ this.state.shoppingList.length > 0 ? <button onClick={this.emptyBasket}>Opróżnij koszyk</button> : "" }
+			</div>		/* jeśli pojawi się jakikolwiek produkct w koszyku, renderuj przycisk opróżniający listę */
 		);
 	}
 }
@@ -42,6 +44,7 @@ class ShopItem extends React.Component {
 			<div>
 				<h3>{this.props.title}</h3>
 				<p>{this.props.description}</p>
+				<span>{this.props.price} zł </span>
 				<button onClick={this.handleClick}>KUP</button>
 			</div>
 		);
